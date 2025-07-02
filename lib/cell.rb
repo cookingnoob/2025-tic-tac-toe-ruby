89e72e1bb443b39295602c0ada5fcc6c368ee7a4
cookingnoob <{ID}+{username}@users.noreply.github.com>
2025-07-02 16:08:37 -0600

    add empty files for classes board, cell, player and bot, add comments for them

---
+++ lib/cell.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# Celda tiene un valor que es initializado como ''
+# tiene un metodo para cambiar y leer ese valor
+# Si tiene un valor que no es '', no se puede cambiar
+# Para cambiar la celda es necesario conocer cual del array se va a cambiar
---
+++ lib/player.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# Clase Jugador que tiene un inicializador del valor X o Y
+# Jugador tiene subclase Bot que escoge el valor opuesto al usuario
+# El metodo de jugar del bot escoge de forma aleatoria un elemento
+# Despues de elegir su elemento, se pasa el turno al jugador
+# Despues del turno del jugador se pasa al bot
