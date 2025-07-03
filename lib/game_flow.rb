599065970ed113146b278c507997f3dcf306fe04
cookingnoob <{ID}+{username}@users.noreply.github.com>
2025-07-03 11:40:09 -0600

    add flow if user types yes it calls ValueSelector class to get user and bot values, if N it exits the program

--- lib/game_flow.rb
+++ lib/game_flow.rb
@@ -1,11 +1,24 @@
 # frozen_string_literal: true
 
+require_relative 'value_selector'
+
 # orchestrates different classes for the game flow
 class GameFlow
   def start_game
     puts 'Want to play Tic Tac Toe? Y/N'
     user_input = gets.chomp
-    puts 'Ok, goodbye' if user_input == 'N'
-    puts 'yeiii, lets play!' if user_input == 'Y'
+    goodbye if user_input == 'N'
+    ask_value if user_input == 'Y'
+  end
+
+  def ask_value
+    puts 'Yei 🥳, lets play!'
+    selector = ValueSelector.new
+    selector.choose_value
+  end
+
+  def goodbye
+    puts 'Ok 😪, goodbye'
+    exit
   end
 end
