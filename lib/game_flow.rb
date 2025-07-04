804046be96dad450f17305cc065ea9acf9a9805a
cookingnoob <{ID}+{username}@users.noreply.github.com>
2025-07-04 11:43:39 -0600

    remove UserValue as it was redundant with ValueSelector, add ValueSelector methods to value method in game_flow

--- lib/game_flow.rb
+++ lib/game_flow.rb
@@ -2,11 +2,21 @@
 
 require_relative 'player'
 require_relative 'board'
+require_relative 'value_selector'
 
 # orchestrates different classes for the game flow
 class GameFlow
+  attr_accessor :player_one, :player_two, :board
+
   def initialize
     @player_one = Player.new
+    @player_two = Player.new
     @board = Board.new
+  end
+
+  def values
+    @player_one.value = ValueSelector.choose_value
+    @player_two.value = ValueSelector.bot_value(@player_one.value)
+    puts "Player value is #{@player_one.value} and bot value is #{@player_two.value}"
   end
 end
