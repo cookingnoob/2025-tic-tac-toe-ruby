5078316a6f64c5bcdb7c53798d203740cadc9bde
cookingnoob <{ID}+{username}@users.noreply.github.com>
2025-07-04 12:55:53 -0600

    remove methods from TurnHandler as its not clear how to use it at the moment, add methods with comments of the turn flow for player and bot

--- lib/game_flow.rb
+++ lib/game_flow.rb
@@ -3,6 +3,7 @@
 require_relative 'player'
 require_relative 'board'
 require_relative 'value_selector'
+require_relative 'turn_handler'
 
 # orchestrates different classes for the game flow
 class GameFlow
@@ -18,5 +19,39 @@ class GameFlow
     @player_one.value = ValueSelector.choose_value
     @player_two.value = ValueSelector.bot_value(@player_one.value)
     puts "Player value is #{@player_one.value} and bot value is #{@player_two.value}"
+    first_turn
+  end
+
+  def first_turn
+    random_number = (rand * 10).to_i
+    @player_one.turn = true if random_number.odd?
+    @player_two.turn = true if random_number.even?
+    match
+  end
+
+  def match
+    player_one_turn if @player_one.turn == true
+    player_two_turn if @player_two.turn == true
+  end
+
+  def player_one_turn
+    puts 'choosing cell'
+    puts add_value
+    puts 'printing new board'
+    puts 'if game not finished'
+    puts 'bot turn'
+  end
+
+  def player_two_turn
+    puts 'choosing cell'
+    puts add_value
+    puts 'printing new board'
+    puts 'if game not finished'
+    puts 'human turn'
+  end
+
+  def add_value
+    puts 'cell available'
+    puts 'adding value to board'
   end
 end
--- lib/turn_handler.rb
+++ lib/turn_handler.rb
@@ -2,29 +2,15 @@
 
 # Responsible for whos turn is it
 class TurnHandler
-  def self.first_turn(player_one, player_two)
-    # random_number = (rand * 10).to_i
-    random_number = 1
-    player_one.turn = true if random_number.odd?
-    player_two.turn = true if random_number.even?
-    puts 'Player one begins the match' if player_one.turn == true
-    puts 'Player two begins the match' if player_two.turn == true
-  end
-
-  def self.current_turn(player_one, player_two)
-    return player_one if player_one.turn
-
-    player_two if player_two.turn
-  end
-
-  def first_turn
-    TurnHandler.first_turn(@player_one, @player_two)
-    @board.paint_board
-    whos_turn
-  end
-
-  def whos_turn
-    current_player = TurnHandler.current_turn(@player_one, @player_two)
-    check_move(current_player)
-  end
 end
+
+# def self.current_turn(player_one, player_two)
+#   return player_one if player_one.turn
+
+#   player_two if player_two.turn
+# end
+
+# def whos_turn
+#   current_player = TurnHandler.current_turn(@player_one, @player_two)
+#   check_move(current_player)
+# end
